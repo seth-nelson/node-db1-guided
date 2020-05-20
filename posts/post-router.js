06d5b325724cc6eb9212621f8ed74192cc1f09ea
Seth Nelson <seth.nelson@outlook.com>
2020-05-19 20:23:25 -0700

    added knex query builder to use as a runner of the SQL.

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,16 +1,47 @@
 const express = require('express');
 
+// 
+const knex = require('../data/db-config.js')
+
 // database access using knex
 const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
 
+// STANDARD METHOD WITH PROMISE
+// router.get('/', (req, res) => {
+//     knex('posts')
+//         .then(posts => {
+//             res.json(posts)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.json(500).json({ message: 'Problem with DB.'})
+//         })
+// });
+
+
+// ASYNC METHOD PREFERRED
+router.get('/', (req, res) => {
+    try {
+        const posts = knex('posts')
+        res.json(posts);
+    }
+    catch (err) {
+        console.log(err)
+        res.json(500).json({ message: 'Problem with DB.'})
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const post = await knex('posts').where({ id });
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: 'Problem with the db.', error: err });
+    }
 });
 
 router.post('/', (req, res) => {
